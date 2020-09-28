9b8f52eb7837d4a2bcf2ac1f719ac8584e33680d
luke-h1 <luke.howsam@yahoo.com>
2020-09-28 19:52:09 +0100

    getting images from API now

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const button = document.getElementById('button')
 const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=7a78372c9666466c223f25edb1c630f5&language=en-US`
-
+const popularResults = document.getElementById('results');
 async function getPopularMovies(){
   const res = await fetch(`${API_URL}`);
   const data = await res.json();
-  console.log(data);
+  showPopularMoviesDOM(data);
+}
+
+
+function showPopularMoviesDOM(data){
+  let output = '';
+  data.results.forEach((item) => { 
+    console.log(item);
+    output += `
+    <div class="card">
+      <div class="card-img--container"></div>
+      <img src="${`https://image.tmdb.org/t/p/w500${item.backdrop_path}`}" alt="" />
+      <div class="card-info--container">
+        <p>${item.original_title}</p>
+        <span class="badge badge-primary">${item.popularity}</span>
+        <span class="badge badge-success">${item.vote_count}</span>
+      </div>
+  </div>
+    `
+  }); 
+  popularResults.innerHTML = output;
+
 }
 
 

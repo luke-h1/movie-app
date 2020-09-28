f8645de5d7c2685cabf8b295c6d3beb160bd1e99
luke-h1 <luke.howsam@yahoo.com>
2020-09-28 20:38:59 +0100

    add single movie functionality & error checking

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const button = document.getElementById('button')
 const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=7a78372c9666466c223f25edb1c630f5&language=en-US`
 const popularResults = document.getElementById('grid');
+const searchBtn = document.getElementById('btn-search')
+
+function   clearFields(){
+  popularResults.innerHTML = ''; 
+}
+
+function showError(message){
+  const errorEl = document.getElementById('error');
+  errorEl.innerHTML = message;
+  setTimeout(() => { 
+    errorEl.innerHTML = '';
+  }, 2000)
+}
+
 async function getPopularMovies(){
   const res = await fetch(`${API_URL}`);
   const data = await res.json();
@@ -9,6 +23,7 @@ async function getPopularMovies(){
 
 
 function showPopularMoviesDOM(data){
+  clearFields();
   let output = '';
   data.results.forEach((item) => { 
     console.log(item);
@@ -27,6 +42,40 @@ function showPopularMoviesDOM(data){
     `
   }); 
   popularResults.innerHTML = output;
+}
+
+async function getMovieQuery(){
+  clearFields(); 
+  const query = document.getElementById('searchbtn')
+  const input = query.value; 
+  if(input === ''){
+    showError('Enter a movie')
+  }else { 
+    const QUERY_URL = `https://api.themoviedb.org/3/search/movie?query=${input}&api_key=7a78372c9666466c223f25edb1c630f5&language=en-US&page=1&include_adult=false`; 
+    const response = await fetch(`${QUERY_URL}`) 
+    const json = await response.json(); 
+    showQueryData(json);   
+  }
+}
+
+function showQueryData(json){
+  console.log(json);
+  let final = '';
+  json.results.forEach((item) => { 
+    final+=`
+     <div class="card">
+      <div class="card-img--container"></div>
+      <img src="${`https://image.tmdb.org/t/p/w500${item.backdrop_path}`}" alt="" />
+      <div class="card-info--container">
+        <p>Title: ${item.original_title}</p>
+        <span class="badge badge-primary">Popularity: ${item.popularity}</span>
+        <span class="badge badge-success">Votes: ${item.vote_count}</span>
+        <span class="badge badge-success">Release Date: ${item.release_date}</span>
+        </div>
+  </div>
+    `
+  }) 
+  popularResults.innerHTML = final;
 
 }
 
@@ -35,3 +84,4 @@ function showPopularMoviesDOM(data){
 
 // EVENT LISTENERS 
 button.addEventListener('click', getPopularMovies)
+searchBtn.addEventListener('click', getMovieQuery)

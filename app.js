ff96e45c9b20479ec2812624b80ccf49eea90613
luke-h1 <luke.howsam@yahoo.com>
2020-09-28 20:03:53 +0100

    update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const button = document.getElementById('button')
 const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=7a78372c9666466c223f25edb1c630f5&language=en-US`
-const popularResults = document.getElementById('results');
+const popularResults = document.getElementById('grid');
 async function getPopularMovies(){
   const res = await fetch(`${API_URL}`);
   const data = await res.json();
@@ -17,10 +17,12 @@ function showPopularMoviesDOM(data){
       <div class="card-img--container"></div>
       <img src="${`https://image.tmdb.org/t/p/w500${item.backdrop_path}`}" alt="" />
       <div class="card-info--container">
-        <p>${item.original_title}</p>
-        <span class="badge badge-primary">${item.popularity}</span>
-        <span class="badge badge-success">${item.vote_count}</span>
-      </div>
+        <p>Title: ${item.original_title}</p>
+        <span class="badge badge-primary">Popularity: ${item.popularity}</span>
+        <span class="badge badge-success">Votes: ${item.vote_count}</span>
+        <span class="badge badge-success">Release Date: ${item.release_date}</span>
+
+        </div>
   </div>
     `
   }); 
